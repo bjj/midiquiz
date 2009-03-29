db99cd89cb4a559ac191cafabde8d9a7e792199f
Ben Jackson <ben@ben.com>
2009-03-29 16:51:14 -0700

    Add seed for random

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <QtGui/QApplication>
+#include <QTime>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
     MainWindow w;
     w.show();
     return a.exec();
